405d38c40cf4f4fff24d1fb0a9b0c10d2278a0a0
Kyle Bjordahl <kyle@lucid.rocks>
2020-01-05 12:55:39 -0800

    fix: don't prepend base and head

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
 import * as core from '@actions/core'
-import {getNxAffectedApps} from './getNxAffectedApps'
+import { getNxAffectedApps } from './getNxAffectedApps'
 
-const {GITHUB_WORKSPACE = '.'} = process.env
+const { GITHUB_WORKSPACE = '.' } = process.env
 
 async function run(): Promise<void> {
-  try {
-    const base = `remotes/origin/${core.getInput('base')}`
-    const head = `remotes/origin/${core.getInput('head')}`
-    core.info(`Getting diff from ${base} to ${head}...`)
-    core.info(`using dir: ${GITHUB_WORKSPACE}`)
+	try {
+		const base = core.getInput('base')
+		const head = core.getInput('head')
+		core.info(`Getting diff from ${base} to ${head || 'HEAD'}...`)
+		core.info(`using dir: ${GITHUB_WORKSPACE}`)
 
-    const apps = getNxAffectedApps({
-      base,
-      head,
-      workspace: GITHUB_WORKSPACE
-    })
+		const apps = getNxAffectedApps({
+			base,
+			head,
+			workspace: GITHUB_WORKSPACE
+		})
 
-    const appsString = JSON.stringify(apps)
+		const appsString = JSON.stringify(apps)
 
-    core.setOutput('affected_apps', appsString)
-    core.exportVariable('NX_AFFECTED_APPS', appsString)
-    core.info(`Found these affected apps: \n ${appsString}`)
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+		core.setOutput('affected_apps', appsString)
+		core.exportVariable('NX_AFFECTED_APPS', appsString)
+		core.info(`Found these affected apps: \n ${appsString}`)
+	} catch (error) {
+		core.setFailed(error.message)
+	}
 }
 
 run()
