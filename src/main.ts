b721b58cca284dfd97d268c1a17e3ac73b46f83d
Kyle Bjordahl <kyle@lucid.rocks>
2020-01-05 20:41:40 -0800

    feat: export base and head to env for later steps

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,33 @@
 import * as core from '@actions/core'
-import {getNxAffectedApps} from './getNxAffectedApps'
+import { getNxAffectedApps } from './getNxAffectedApps'
 
 export async function run(workspace: string = '.'): Promise<void> {
-    try {
-        const {GITHUB_WORKSPACE = workspace} = process.env
-        const base = core.getInput('base')
-        const head = core.getInput('head')
-        core.info(`Getting diff from ${base} to ${head || 'HEAD'}...`)
-        core.info(`using dir: ${GITHUB_WORKSPACE}`)
+	try {
+		const { GITHUB_WORKSPACE = workspace } = process.env
+		const base = core.getInput('base')
+		const head = core.getInput('head')
 
-        const apps = getNxAffectedApps({
-            base,
-            head,
-            workspace: GITHUB_WORKSPACE
-        })
+		// save base and head to env for later steps
+		core.exportVariable('NX_BASE', base || 'HEAD~1')
+		core.exportVariable('NX_HEAD', head || 'HEAD')
 
-        const appsString = JSON.stringify(apps)
+		core.info(`Getting diff from ${base} to ${head || 'HEAD'}...`)
+		core.info(`using dir: ${GITHUB_WORKSPACE}`)
 
-        core.setOutput('affected_apps', appsString)
-        core.exportVariable('NX_AFFECTED_APPS', appsString)
-        core.info(`Found these affected apps: \n ${appsString}`)
-    } catch (error) {
-        core.setFailed(error.message)
-    }
+		const apps = getNxAffectedApps({
+			base,
+			head,
+			workspace: GITHUB_WORKSPACE
+		})
+
+		const appsString = JSON.stringify(apps)
+
+		core.setOutput('affected_apps', appsString)
+		core.exportVariable('NX_AFFECTED_APPS', appsString)
+		core.info(`Found these affected apps: \n ${appsString}`)
+	} catch (error) {
+		core.setFailed(error.message)
+	}
 }
 
 run()
