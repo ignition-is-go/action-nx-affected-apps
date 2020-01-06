bc483d7e48703211826b0951a4e0d620f77d46c4
Kyle Bjordahl <kyle@lucid.rocks>
2020-01-05 20:10:40 -0800

    fix: add contingency for no affected apps

--- src/getNxAffectedApps.ts
+++ src/getNxAffectedApps.ts
@@ -1,59 +1,63 @@
-import {execSync} from 'child_process'
+import { execSync } from 'child_process'
 import * as core from '@actions/core'
 
-export function getNxAffectedApps({base, head, workspace}: Props): string[] {
-    const args = `${base ? `--base=${base}` : ''} ${
-        head ? `--head=${head}` : ''
-    }`
-    let result
-    try {
-        const cmd = `npm run nx -- affected:apps ${args}`
-        core.info(`Attempting npm script: ${cmd}`)
-        result = execSync(cmd, {
-            cwd: workspace
-        }).toString()
-    } catch (e) {
-        core.info(`first attempt failed: ${e.message}`)
-        try {
-            const cmd = `./node_modules/.bin/nx affected:apps ${args}`
-            core.info(`Attempting from node modules: ${cmd}`)
-            result = execSync(cmd, {
-                cwd: workspace
-            }).toString()
-        } catch (e2) {
-            core.info(`second attempt failed: ${e2.message}`)
-            const cmd = `nx affected:apps ${args}`
-            core.info(`Attempting global npm bin: ${cmd}`)
-            result = execSync(cmd, {
-                cwd: workspace
-            }).toString()
-        }
-    }
+export function getNxAffectedApps({ base, head, workspace }: Props): string[] {
+	const args = `${base ? `--base=${base}` : ''} ${
+		head ? `--head=${head}` : ''
+		}`
+	let result
+	try {
+		const cmd = `npm run nx -- affected:apps ${args}`
+		core.debug(`Attempting npm script: ${cmd}`)
+		result = execSync(cmd, {
+			cwd: workspace
+		}).toString()
+	} catch (e) {
+		core.debug(`first attempt failed: ${e.message}`)
+		try {
+			const cmd = `./node_modules/.bin/nx affected:apps ${args}`
+			core.debug(`Attempting from node modules: ${cmd}`)
+			result = execSync(cmd, {
+				cwd: workspace
+			}).toString()
+		} catch (e2) {
+			try {
+				core.debug(`second attempt failed: ${e2.message}`)
+				const cmd = `nx affected:apps ${args}`
+				core.debug(`Attempting global npm bin: ${cmd}`)
+				result = execSync(cmd, {
+					cwd: workspace
+				}).toString()
+			}
+			catch (e3) {
+				core.debug(`third attempt failed: ${e3.message}`)
+				throw Error(
+					'Could not run NX cli...Did you install it globally and in your project? Also, try adding this npm script: "nx":"nx"'
+				)
+			}
+		}
+	}
 
-    if (!result) {
-        throw Error(
-            'Could not run NX cli...Did you install it globally and in your project? Also, try adding this npm script: "nx":"nx"'
-        )
-    }
+	if (!result) {
+		return []
+	}
 
-    core.info(`Affected Apps output: \n${result}`)
+	if (!result.includes('Affected apps:')) {
+		throw Error(`NX Command Failed: ${result}`)
+	}
 
-    if (!result.includes('Affected apps:')) {
-        throw Error(`NX Command Failed: ${result}`)
-    }
+	const apps = result
+		.split('Affected apps:')[1]
+		.trim()
+		.split('- ')
+		.map(x => x.trim())
+		.filter(x => x.length > 0)
 
-    const apps = result
-        .split('Affected apps:')[1]
-        .trim()
-        .split('- ')
-        .map(x => x.trim())
-        .filter(x => x.length > 0)
-
-    return apps || []
+	return apps || []
 }
 
 interface Props {
-    base?: string
-    head?: string
-    workspace: string
+	base?: string
+	head?: string
+	workspace: string
 }
